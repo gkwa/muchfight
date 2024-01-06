411c1b555079a5d3c262d257c4818739c1a17168
Taylor Monacelli <taylormonacelli@gmail.com>
2024-01-05 20:30:01 -0500

    save scripts

---
+++ 2d.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+mdfind -onlyin /Users/mtm/pdev 'kMDItemFSContentChangeDate >= $time.now(-172800) && kMDItemFSName == "*.go"' |
+    xargs -d '\n' -a - rg -l Command |
+    xargs -d '\n' -a - rg -l boom
